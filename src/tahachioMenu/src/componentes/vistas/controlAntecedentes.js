b1410bf2e8ddab5ad4745ac8ff4255c3f45687af
isaac-coronel <36717914+isaac-coronel@users.noreply.github.com>
2022-04-06 21:44:05 -0400

    crypto mas auditoria
    cambios con el crypto y el guardado de auditoria

--- src/tahachioMenu/src/componentes/vistas/controlAntecedentes.js
+++ src/tahachioMenu/src/componentes/vistas/controlAntecedentes.js
@@ -1,6 +1,12 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+/* eslint-disable prettier/prettier */
 import React, { useState, useEffect } from 'react'
 import { Text, View, StyleSheet, Button } from 'react-native'
 import { BarCodeScanner } from 'expo-barcode-scanner'
+// eslint-disable-next-line no-unused-vars
+import CryptoES from 'crypto-js'
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null)
@@ -12,7 +18,35 @@ export default function App() {
       setHasPermission(status === 'granted')
     })()
   }, [])
+  function setAuditoriaConsulta(detalles) {
+    const myHeaders = new Headers()
+    myHeaders.append('content-type', 'application/json')
 
+    const raw = JSON.stringify({
+      idUsuario: 1,
+      detalles,
+    })
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    }
+
+    fetch(
+      'https://node-mysql-isak.herokuapp.com/api/setHistorialAuditoria',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((error) => {
+        console.log('error', error)
+        alert('Usuario sin registro de antecedentes')
+      })
+  }
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true)
     // useEffect(() => {
@@ -39,6 +73,34 @@ export default function App() {
       .then((response) => response.json())
       .then((result) => {
         console.log(result.result[0][0])
+        try {
+          const datos =
+            result.result[0][0].pers_nombre +
+            ' ' +
+            result.result[0][0].pers_apellido +
+            ' / ' +
+            result.result[0][0].pers_ci +
+            ' / ' +
+            result.result[0][0].ante_descripcion
+          setAuditoriaConsulta(datos)
+          const CryptoJS = require('crypto-js')
+
+          // Encrypt
+          const ciphertext = CryptoJS.AES.encrypt(
+            'my message',
+            'secret key 123'
+          ).toString()
+
+          // Decrypt
+          const bytes = CryptoJS.AES.decrypt(ciphertext, 'secret key 123')
+          const originalText = bytes.toString(CryptoJS.enc.Utf8)
+          console.log(ciphertext)
+          console.log(originalText) // 'my message'
+        } catch (error) {
+          console.log(error)
+        }
+        // eslint-disable-next-line spaced-comment
+        //mjn
         alert(
           `Datos de los antecedentes 
           Nombre: ${result.result[0][0].pers_nombre}
